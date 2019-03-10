6cb1502dfb13f88ca3dccea71aec8099a677d22d
Andrei Gavrila <andrei.gavrila@hotmail.com>
2019-03-10 21:00:56 +0200

    added the unit test library

---
+++ exgths.tests.median.test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace exgths.tests.median.test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
