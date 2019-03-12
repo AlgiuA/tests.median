71d92d79b0472225323a496bf00b1270a783b2df
Alin Algiu <algiu.alin@gmail.com>
2019-03-12 19:00:00 +0200

    - problem resolved

--- exgths.tests.median.test/UnitTest1.cs
+++ exgths.tests.median.test/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace exgths.tests.median.test
@@ -6,11 +7,35 @@ namespace exgths.tests.median.test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test1()
         {
             Class1 c = new Class1();
 
-            Assert.AreEqual(2, c.Median(new int[] { 1, 2, 3 }));
+            Assert.AreEqual(6.5, c.Median(new int[] { 6, 7, 8, 9, 1, 3 }));
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            Class1 c = new Class1();
+
+            Assert.AreEqual(9, c.Median(new int[] { 1, 5, 2, 7, 9, 10, 25, 21, 52 }));
+        }
+
+        [TestMethod]
+        public void Test_Throws_NullException()
+        {
+            Class1 c = new Class1();
+
+            Assert.ThrowsException<ArgumentNullException>(() => c.Median(null));
+        }
+
+        [TestMethod]
+        public void Test_Throws_ArgumentException()
+        {
+            Class1 c1 = new Class1();
+
+            Assert.ThrowsException<ArgumentException>(() => c1.Median(new int[] { }), "Array shouldn't be empty!");
         }
     }
 }
--- exgths.tests.median/Class1.cs
+++ exgths.tests.median/Class1.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace exgths.tests.median
 {
     public class Class1
     {
-        public int Median(int[] array)
+        public double Median(int[] array)
         {
-            return 2;
+            if (null == array)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            if (array.Length == 0)
+            {
+                throw new ArgumentException("Array shouldn't be empty!");
+            }
+
+            if (array.Length == 1)
+            {
+                return array[0];
+            }
+
+            List<int> list = array.OrderBy(x => x).ToList();
+
+            if (list.Count % 2 == 1)
+            {
+                return list.ElementAt(list.Count / 2);
+            }
+            return (list.ElementAt((list.Count / 2) - 1) + list.ElementAt(list.Count / 2)) / 2.0;
+
         }
     }
 }
